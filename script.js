c19879aea78710c701dc247beada1a9f68b3d0a4
PrabulPRK <99730122+PrabulPRK@users.noreply.github.com>
2022-02-16 09:57:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function buttonclick(val) 
+{
+		document.getElementById(	"screen"	).value=document.getElementById(	"screen"	).value+val;
+}
+function clearDisplay()
+{
+				document.getElementById("screen").value=""
+}
+function equalClick()
+{
+				var text=document.getElementById("screen").value
+				var result=eval(text)
+				document.getElementById("screen").value=result;
+}
+function deleteSingleNum()
+{
+				document.getElementById("screen").value=document.getElementById("screen").value.substring(0,document.getElementById("screen").value.length-1 )
+}
+
+
+
+
